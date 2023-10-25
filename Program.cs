0156590bae0728ab0f380ca6853637742390e244
Tiago Ferrari <tiago-projects@outlook.com>
2023-10-24 23:47:08 -0300

    chore(project): first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Start();
+    }
+
+    static void Start()
+    {
+        int time = 10;
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+
+        }
+    }
+}
