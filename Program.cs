baf43cb6e53e35c9d2f66bae24027638f1129cab
Tiago Ferrari <tiago-projects@outlook.com>
2023-10-25 00:54:12 -0300

    feat(stopwatch): added  features

--- Program.cs
+++ Program.cs
@@ -2,12 +2,50 @@
 {
     private static void Main(string[] args)
     {
-        Start();
+        Menu();
     }
 
-    static void Start()
+    static void Menu()
     {
-        int time = 10;
+        Console.Clear();
+        Console.WriteLine("S = Segundo => 10s = 10 segundos");
+        Console.WriteLine("M = Minuto => 1m = 1 minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+        int multiplier = 1;
+
+        if (type == 'm')
+        {
+            multiplier = 60;
+        }
+
+        if (time == 0)
+        {
+            System.Environment.Exit(0);
+        }
+
+        PreStart(time * multiplier);
+
+    }
+
+    static void PreStart(int time)
+    {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Set...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Go...");
+        Thread.Sleep(2500);
+
+        Start(time);
+    }
+    static void Start(int time)
+    {
         int currentTime = 0;
 
         while (currentTime != time)
@@ -18,5 +56,10 @@
             Thread.Sleep(1000);
 
         }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado!");
+        Thread.Sleep(2500);
+        Menu();
     }
 }
